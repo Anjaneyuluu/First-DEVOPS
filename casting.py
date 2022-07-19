5867bedbf52c49bfab4c24935746a89ada0fe67b
Anjaneyulu <Anjaneyulu.git@gmail.com>
2022-07-19 10:49:26 +0530

    this is a python application

---
+++ casting.py
@@ -0,0 +1,8 @@
+#if you specify the data type of thr variable ,this can be done with casting
+x = str(3) # x can be '3'
+y = int(3)  # y can be 3
+z = float(3) # z can be 3.0
+
+print(x)
+print(y)
+print(z)
