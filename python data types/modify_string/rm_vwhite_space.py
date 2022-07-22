f82ca1932bc605755267514b26f821e63e6b5f4c
Anjaneyulu <Anjaneyulu.git@gmail.com>
2022-07-22 23:54:14 +0530

    split,remove,formating,lower,upper case,replace,remove,concatenating strings

---
+++ python data types/modify_string/rm_vwhite_space.py
@@ -0,0 +1,9 @@
+# here whitespace is the space between before and/or actual text, and very often you can remove this space.
+
+#  the 'strip()'method used to remove the whitespace
+
+a = "   Hello, World!   "
+
+print(a.strip())
+
+
